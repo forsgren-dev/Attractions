e588ee6200b60c66d1f8f3fa744353eaba004865
Erik Forsgren <forsgren.dev@gmail.com>
2026-09-01 09:25:07 +0200

    first commit

---
+++ Models/Attraction.cs
@@ -0,0 +1,13 @@
+﻿using Seido.Utilities.SeedGenerator;
+
+namespace Models;
+
+public class Attraction : IAttraction
+{
+
+    public virtual Guid AttractionId { get; set; }
+    public string AttractionName { get; set; }
+
+}
+
+
---
+++ Models/IAttraction.cs
@@ -0,0 +1,11 @@
+﻿namespace Models;
+
+
+public interface IAttraction
+{
+    public Guid AttractionId { get; set; }
+    public string AttractionName { get; set; }
+
+}
+
+
