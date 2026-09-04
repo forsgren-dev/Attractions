1b7928caea6066d3a95af06abffd033cd3bf49b6
Erik Forsgren <forsgren.dev@gmail.com>
2026-09-04 13:28:44 +0200

    Fix name concistency

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using Seido.Utilities.SeedGenerator;
+
+namespace Models;
+
+public class Category : ICategory
+{
+
+    public virtual Guid CategoryId { get; set; }
+
+    public CategoryType CategoryType { get; set; }
+}
---
+++ Models/ICategory.cs
@@ -0,0 +1,27 @@
+﻿namespace Models;
+
+
+public interface ICategory
+{
+    
+    public Guid CategoryId { get; set; }
+
+    public CategoryType CategoryType { get; set; }
+
+}
+
+public enum CategoryType
+{
+    Museum,
+    Park,
+    Restaurant,
+    Historical,
+    Religious,
+    Art,
+    Architecture,
+    Nature,
+    Amusement,
+    Shopping,
+}
+
+
