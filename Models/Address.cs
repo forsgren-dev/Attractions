613fd6c0376b103465f6e9e36eb1d0d4b55c597d
Erik Forsgren <forsgren.dev@gmail.com>
2026-09-04 10:52:50 +0200

    Add IEquatable to Address model

--- Models/Address.cs
+++ Models/Address.cs
@@ -2,7 +2,7 @@
 
 namespace Models;
 
-public class Address : IAddress
+public class Address : IAddress, IEquatable<Address>
 {
 
     public virtual Guid AddressId { get; set; }
@@ -11,6 +11,32 @@ public class Address : IAddress
     public string PostalCode { get; set; }
     public virtual ICity City { get; set; }
     public virtual ICountry Country { get; set; }
+
+
+    public bool Equals(Address? other)
+    {
+        if (other is null) return false;
+        
+        return string.Equals(Street, other.Street, StringComparison.OrdinalIgnoreCase)
+            && string.Equals(PostalCode, other.PostalCode, StringComparison.OrdinalIgnoreCase)
+            && City?.CityId == other.City?.CityId
+            && Country?.CountryId == other.Country?.CountryId;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as Address);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(
+            Street?.ToLowerInvariant(),
+            PostalCode?.ToLowerInvariant(),
+            City?.CityId,
+            Country?.CountryId);
+
+    }
 
 }
 
