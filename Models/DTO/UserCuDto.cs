c484e7b44b7fc767b9f7aa114549f16e9b548d63
Erik Forsgren <forsgren.dev@gmail.com>
2026-09-18 10:53:28 +0200

    Adding dto for comments outside attractions

--- Models/DTO/UserCuDto.cs
+++ Models/DTO/UserCuDto.cs
@@ -3,6 +3,9 @@ namespace Models.DTO;
 
 public class UserCuDto
 {
+    #if DEBUG
+    public string ConnectionString { get; set; }
+    #endif
     public Guid? UserId { get; set; }
     public string UserName { get; set; }
     public List<Guid?> CommentsId { get; set; } = null;
