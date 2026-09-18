d2d09d75357b0c6eb9a73a5f85589269eb669b6c
Erik Forsgren <forsgren.dev@gmail.com>
2026-09-18 10:45:47 +0200

    Upd comment

--- Models/DTO/AttractionDto.cs
+++ Models/DTO/AttractionDto.cs
@@ -12,10 +12,9 @@ public class AttractionDto
     public  AttractionAddressDto Address { get; set; }
     public List<string> Categories { get; set; } = new();
 
-    //Hittade denna metod för att dölja "comments": [] i json-svaret när kommentarer inte ska visas
+    //Hittade denna metod för att dölja sjävla '"comments": []' i json-svaret när kommentarer inte ska visas
     [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public List<CommentDto> Comments { get; set; }
-
 
 
 }
